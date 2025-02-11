bb0b6adff747d9e9c0cc7a3cdf1a1132c9afae03
RavenNight5 <182237149+RavenNight5@users.noreply.github.com>
2025-02-11 21:18:54 +0000

    Cleared up the Game class.

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -33,38 +33,7 @@ namespace CMP1903_A1_2324
                 }
             }
 
-
-            //Console.WriteLine("The face value of the die just rolled is: " + FaceValue + " " + DieVisual);
-
         }
 
-        // This part of the code rolls the dice and then calculates the sum, reporting the total.
-        //public int RollAndReportTotal()
-        //{
-        //    int total = 0;
-
-        //    foreach (Die die in dice)
-        //    {
-        //        die.Roll(); // This rolls each of the die.
-
-        //        total += die.FaceValue; // This adds the current face value of the die to the total.
-        //    }
-        //    return total;
-        //}
     }
-
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        // This creates a new instance of the Game itself.
-    //        Game game = new Game();
-
-    //        // This rolls the dice and reports the total value.
-    //        int total = game.RollAndReportTotal();
-
-    //        // Displays the total value.
-    //        Console.WriteLine("The total of the three dice rolls is: " + total);
-    //    }
-    //}
 }
